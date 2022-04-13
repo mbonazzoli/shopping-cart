3dd8471ea92f4c7439854435763b53dde78f53b4
mbonazzoli <mtlb215@gmail.com>
2022-04-12 21:48:20 -0700

    cleanup

--- cart.jsx
+++ cart.jsx
@@ -167,8 +167,6 @@ const Products = (props) => {
   };
   // TODO: implement the restockProducts function
   const restockProducts = (url) => {
-    debugger;
-    console.log(`data returned from fetch: ${JSON.stringify(data)}`)
     doFetch(url);
     let newItems = data.data.map((item) => {
       let {name, country, cost, instock} = item.attributes;
